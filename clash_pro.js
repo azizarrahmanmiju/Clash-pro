5ffa3326e445f2c4db64db8f2fad13519b74241b
azizarrahman <azizarrahman01032003@gmail.com>
2024-11-01 00:21:36 +0600

    INitialy Display Random Nuber Showing

--- clash_pro.js
+++ clash_pro.js
@@ -1,4 +1,28 @@
 
 function play() {
-    console.log("PLaying...");
+    hidecontainer('home-Screen');
+    showcontainer('playground');
+    const number = generaterandomalphabet();
+    document.getElementById('display-word').innerText = number;
+
+
+}
+
+function hidecontainer(id) {
+    document.getElementById(id).classList.add('hidden');
+}
+function showcontainer(id) {
+    document.getElementById(id).classList.remove('hidden');
+}
+
+function generaterandomalphabet() {
+    const alphabetstring = 'abcdefghijklmnopqrstuvwxyz';
+    const singlealphabet = alphabetstring.split('');
+    const randomnuber = Math.random() * 25;
+    const index = Math.round(randomnuber);
+    const alphabet = alphabetstring[index];
+    return alphabet;
+
+
+
 }
