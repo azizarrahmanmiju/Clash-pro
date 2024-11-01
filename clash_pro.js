a5527e17e0b16e4f611598073a20fd6eba09abe7
azizarrahman <azizarrahman01032003@gmail.com>
2024-11-02 01:51:39 +0600

    keyboard auto generate and urrect value bg remove

--- clash_pro.js
+++ clash_pro.js
@@ -9,6 +9,7 @@ function play() {
     setbackgroundcolor(number);
 
 
+
 }
 
 function hidecontainer(id) {
@@ -23,6 +24,11 @@ function setbackgroundcolor(elementid) {
     component.classList.add('bg-orange-400');
 
 }
+function removebackground(elementid) {
+    const component = document.getElementById(elementid);
+    component.classList.remove('bg-orange-400');
+
+}
 
 function generaterandomalphabet() {
     const alphabetstring = 'abcdefghijklmnopqrstuvwxyz';
@@ -31,7 +37,22 @@ function generaterandomalphabet() {
     const index = Math.round(randomnuber);
     const alphabet = alphabetstring[index];
     return alphabet;
+}
 
+document.addEventListener('keyup', handlekeyup);
+function handlekeyup(event) {
+    const byplayerpressed = event.key;
+    console.log(byplayerpressed);
+    const expectedvalue = document.getElementById('display-word').innerText.toLowerCase();
+    if (expectedvalue == byplayerpressed) {
+        console.log('correct', byplayerpressed, expectedvalue);
+        play();
+        removebackground(expectedvalue);
+
+    } else {
+        console.log('incorrect', byplayerpressed, expectedvalue)
+        play();
+    }
 
 
 }
